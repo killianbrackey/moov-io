1f08586e3ff0a2c96ccffe97e26718ae7cb0f9ae
Josh Sadler <joshsadler@me.com>
2020-07-21 14:37:00 -0500

    split investors list to own page and alphabetize

--- themes/moov/assets/js/base.js
+++ themes/moov/assets/js/base.js
@@ -243,11 +243,13 @@ function signupError() {
 	signupForm.innerHTML = "Oops! There was a problem.";
 }
 
-signupForm.addEventListener("submit", function(event) {
-	event.preventDefault();
-	var data = new FormData(signupForm);
-	formPost(signupForm.method, signupForm.action, data, signupSuccess, signupError);
-});
+if (signupForm) {
+	signupForm.addEventListener("submit", function(event) {
+		event.preventDefault();
+		var data = new FormData(signupForm);
+		formPost(signupForm.method, signupForm.action, data, signupSuccess, signupError);
+	});
+}
 
 // Waitlist dialog
 var dialog = document.querySelector('#waitlist-dialog');
@@ -260,75 +262,12 @@ for (var i = 0; i < dialogLaunchers.length; i++) {
 		dialog.setAttribute('aria-hidden', false);
 	})
 }
-dialogCloser.addEventListener('click', function(event) {
-	event.preventDefault();
-	dialog.classList.remove('in');
-	dialog.setAttribute('aria-hidden', true);
-	signupForm.style.display = "";
-	signupStatus.innerHTML = "";
-});
-
-// Investors dialog
-var investorsDialog = document.querySelector('#investors-dialog');
-var investorsDialogLaunchers = document.querySelectorAll('.opens-investors-dialog');
-var investorDialogCloser = document.querySelector('.closes-investor-dialog');
-for (var i = 0; i < investorsDialogLaunchers.length; i++) {
-	investorsDialogLaunchers[i].addEventListener('click', function(event) {
+if (dialogCloser) {
+	dialogCloser.addEventListener('click', function(event) {
 		event.preventDefault();
-		investorsDialog.classList.add('in');
-		investorsDialog.setAttribute('aria-hidden', false);
-		document.body.style.overflowY = 'hidden';
-	})
+		dialog.classList.remove('in');
+		dialog.setAttribute('aria-hidden', true);
+		signupForm.style.display = "";
+		signupStatus.innerHTML = "";
+	});
 }
-investorDialogCloser.addEventListener('click', function(event) {
-	event.preventDefault();
-	document.body.style.overflowY = '';
-	investorsDialog.classList.remove('in');
-	investorsDialog.setAttribute('aria-hidden', true);
-});
-
-
-var quotes;
-
-document.addEventListener('DOMContentLoaded', function() {
-	quotes = new Splide( '.splide', { 
-		arrows: false, 
-		pagination: false, 
-		speed: 1000, 
-		waitForTransition: false,
-		type: 'loop'
-	}).mount();
-
-	quotes.on('mounted', function(){
-		document.querySelector('.quote-nav .numbers .total-slides').textContent = document.querySelectorAll('.splide__slide').length;
-	});
-
-	quotes.on('move', function(){
-		var current = quotes.index + 1;
-		if(current <= 9) current = '0' + current.toString();
-		document.querySelector('.quote-nav .numbers .number-left').textContent = current;
-		var logos = document.querySelectorAll('.investor-logos .image');
-		for (var i = 0; i < logos.length; i++) {
-			logos[i].classList.remove('active');
-		};
-		document.querySelectorAll('.investor-logos .image')[quotes.index].classList.add('active')
-	});
-});
-
-document.querySelector('#prev-next').addEventListener('click', function() {
-	quotes.go('+1');
-});
-
-document.querySelector('#prev-quote').addEventListener('click', function() {
-	quotes.go('-1');
-});
-
-var investor_logos = document.querySelectorAll('.investor-logos .image');
-for (var i = 0; i < investor_logos.length; i++) {
-	investor_logos[i].addEventListener('click', function(){
-		var child = this;
-		var parent = child.parentNode;
-		var index = Array.prototype.indexOf.call(parent.children, child);
-		quotes.go(index);
-	});
-};
